80cbab52a48ca226ca92dc05ca1616737416ee5f
Octavio Olea <oleaoctavio23@gmail.com>
2022-02-25 17:10:35 -0600

    Creada tabla de yolitia por medio de spring recuerden cambiar a la segunda ejecución la linea que dice update a none en el application.properties para que no se duplique sus relaciones.

---
+++ yolitia/src/main/java/com/yolitia/demo/model/Peluches.java
@@ -0,0 +1,47 @@
+package com.yolitia.demo.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+import lombok.Data;
+
+@Entity
+@Table(name= "peluches")
+@Data
+public class Peluches {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idPeluches", nullable = false)
+	
+	
+	private Integer id;
+	
+	@Column(name = "Productos_id", nullable = false)
+	
+	
+	private Integer Productos_id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id")
+	private Productos productos;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "peluche")
+	private List<Personalizacion> personalizacion;
+	
+	
+	
+	
+
+}
