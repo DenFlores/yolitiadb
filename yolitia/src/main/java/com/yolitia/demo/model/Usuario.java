52b9c3e960e8f6d1ea8936817d830d2a22da007c
Deny Flores <deny.flores03@gmail.com>
2022-02-25 16:05:41 -0600

    "Primeros avances de conexión a base de datos"

---
+++ yolitia/src/main/java/com/yolitia/demo/model/Usuario.java
@@ -0,0 +1,37 @@
+package com.yolitia.demo.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario", nullable = false)
+	private Integer id_usuario;
+	
+	@Column(name = "nombre", nullable = false, length = 255)
+	private String nombre;
+	
+	@Column(name = "correo", nullable = false, length = 255)
+	private String correo;
+	
+	@Column(name = "contraseña", nullable = false, length = 255)
+	private String contraseña;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario")
+	private List<Reseñas> reseñas;
+}
